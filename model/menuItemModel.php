9b2cd37565dccbf6866fe9765079d16203ea2ac7
NazmusSakib24 <ahmednazmussakib629@gmail.com>
2026-05-15 00:33:31 +0600

    added menu item create, repeat, update and delete

---
+++ model/menuItemModel.php
@@ -0,0 +1,92 @@
+<?php
+
+    require_once('db.php');
+
+    function addMenuItem($menuItem){
+
+        $con = getConnection();
+
+        $sql = "INSERT INTO menu_items VALUES(
+                    null,
+                    '{$menuItem['restaurant_id']}',
+                    '{$menuItem['name']}',
+                    '{$menuItem['description']}',
+                    '{$menuItem['price']}',
+                    null,
+                    CURRENT_TIMESTAMP
+                )";
+
+        if(mysqli_query($con, $sql)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    function getAllMenuItems(){
+        $con = getConnection();
+
+        $sql = "SELECT * FROM menu_items";
+
+        $result = mysqli_query($con, $sql);
+
+        $menuItems = [];
+
+        while($row = mysqli_fetch_assoc($result)){
+            $menuItems[] = $row;
+        }
+
+        return $menuItems;
+    }
+
+
+    function deleteMenuItem($id){
+        $con = getConnection();
+
+        $sql = "DELETE FROM menu_items WHERE id='{$id}'";
+
+        if(mysqli_query($con, $sql)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    function getMenuItemById($id){
+
+        $con = getConnection();
+
+        $sql = "SELECT * FROM menu_items WHERE id='{$id}'";
+
+        $result = mysqli_query($con, $sql);
+
+        $menuItem = mysqli_fetch_assoc($result);
+
+        return $menuItem;
+
+    }
+    
+    
+
+    function updateMenuItem($menuItem){
+
+        $con = getConnection();
+
+        $sql = "UPDATE menu_items SET
+                restaurant_id = '{$menuItem['restaurant_id']}',
+                name = '{$menuItem['name']}',
+                description = '{$menuItem['description']}',
+                price = '{$menuItem['price']}'
+                WHERE id = '{$menuItem['id']}'";
+
+            if(mysqli_query($con, $sql)){
+                return true;
+            }
+            else{
+                return false;
+            }
+
+}
+?>
---
+++ view/menuItemsView.php
@@ -0,0 +1,84 @@
+<?php
+
+    require_once('../model/restaurantModel.php');
+    require_once('../model/menuItemModel.php');
+    
+    $restaurants = getAllRestaurant();
+    $menuItems = getAllMenuItems();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Menu Items</title>
+</head>
+<body>
+
+    <h1>Menu Item Management</h1>
+
+    <form action="../controller/menuItemController.php" method="post">
+
+        Restaurant:
+        <select name="restaurant_id">
+
+            <?php foreach($restaurants as $r){ ?>
+
+                <option value="<?= $r['id'] ?>">
+                    <?= $r['name'] ?>
+                </option>
+
+            <?php } ?>
+
+        </select>
+
+        <br><br>
+
+        Item Name:
+        <input type="text" name="name">
+        <br><br>
+
+        Description:
+        <textarea name="description"></textarea>
+        <br><br>
+
+        Price:
+        <input type="text" name="price">
+        <br><br>
+
+        <input type="submit" name="add_menu_item" value="Add Menu Item">
+
+    </form>
+    <br><br>
+
+    <table border="1">
+
+        <tr>
+            <th>ID</th>
+            <th>Restaurant ID</th>
+            <th>Item Name</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Action</th>
+        </tr>
+
+        <?php foreach($menuItems as $m){ ?>
+            <tr>
+                <td><?= $m['id'] ?></td>
+                <td><?= $m['restaurant_id'] ?></td>
+                <td><?= $m['name'] ?></td>
+                <td><?= $m['description'] ?></td>
+                <td><?= $m['price'] ?></td>
+                <td>
+                    <a href="editMenuItem.php?id=<?= $m['id'] ?>">Edit
+                    </a> |
+
+                    <a href="../controller/menuItemController.php?id=<?= $m['id'] ?>">Delete
+                    </a>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
+
+</body>
+</html>
